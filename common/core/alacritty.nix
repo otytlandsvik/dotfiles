350e216b1dbf3a93462acc20bf25a3a774a36a88
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2024-05-22 00:23:25 +0200

    fix: cleanup alacritty color cfg

--- common/core/alacritty.nix
+++ common/core/alacritty.nix
@@ -1,39 +1,6 @@
-{ config, ... }:
+{ ... }:
 {
   programs.alacritty = {
     enable = true;
-    settings = {
-      # Use nix-colors scheme
-      # colors = with config.colorScheme.palette; {
-      #   bright = {
-      #     black = "0x${base02}";
-      #     blue = "0x${base0D}";
-      #     cyan = "0x${base0C}";
-      #     green = "0x${base0B}";
-      #     magenta = "0x${base0E}";
-      #     red = "0x${base08}";
-      #     white = "0x${base05}";
-      #     yellow = "0x${base09}";
-      #   };
-      #   cursor = {
-      #     cursor = "0x${base05}";
-      #     text = "0x${base05}";
-      #   };
-      #   normal = {
-      #     black = "0x${base01}";
-      #     blue = "0x${base0D}";
-      #     cyan = "0x${base0C}";
-      #     green = "0x${base0B}";
-      #     magenta = "0x${base0E}";
-      #     red = "0x${base08}";
-      #     white = "0x${base05}";
-      #     yellow = "0x${base0A}";
-      #   };
-      #   primary = {
-      #     background = "0x${base01}";
-      #     foreground = "0x${base05}";
-      #   };
-      # };
-    };
   };
 }
