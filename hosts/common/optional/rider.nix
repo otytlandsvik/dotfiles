c16694202955cab02c9edc7bded0040f48ceed5b
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2024-12-04 10:07:25 +0100

    fix: rider overlay

--- hosts/common/optional/rider.nix
+++ hosts/common/optional/rider.nix
@@ -1,8 +1,33 @@
 { pkgs, ... }:
-# TODO: Add an overlay here
 # TODO: Declare the .ideavim config here (write to home)
 {
-  home.packages = [ pkgs.jetbrains.rider ];
+  # TODO: Remove once fix is merged
+  nixpkgs.overlays =
+    let
+      rider-overlay = (
+        final: prev: {
+          rider = prev.jetbrains.rider.overrideAttrs (attrs: {
+            postInstall = ''
+              cd $out/rider
+
+              ls -d $PWD/plugins/cidr-debugger-plugin/bin/lldb/linux/*/lib/python3.8/lib-dynload/* |
+              xargs patchelf \
+                --replace-needed libssl.so.10 libssl.so \
+                --replace-needed libcrypto.so.10 libcrypto.so \
+                --replace-needed libcrypt.so.1 libcrypt.so
+
+              for dir in lib/ReSharperHost/linux-*; do
+                rm -rf $dir/dotnet
+                ln -s ${prev.dotnet-sdk_7.unwrapped}/share/dotnet $dir/dotnet
+              done
+            '';
+          });
+        }
+      );
+    in
+    [ rider-overlay ];
+
+  home.packages = [ pkgs.rider ];
 
   # Allow old dotnet versions
   nixpkgs.config.permittedInsecurePackages = [
