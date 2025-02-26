b36ca54866c9a818f3ab69fac89722a3955f98f7
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2025-02-26 13:59:12 +0100

    feat(git): better defaults

--- hosts/common/core/git.nix
+++ hosts/common/core/git.nix
@@ -5,8 +5,49 @@
     userName = "Ole Tytlandsvik";
     userEmail = "ole.tytlandsvik@gmail.com";
 
+    # Better defaults, inspired by https://blog.gitbutler.com/how-git-core-devs-configure-git
     extraConfig = {
       init.defaultBranch = "master";
+
+      # Format branch names in columns
+      column.ui = "auto";
+
+      # Don't sort branches and tags alphabetically
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+
+      diff = {
+        # Better algorithm for 'git diff'
+        algorithm = "histogram";
+        # Distinguish moved code with separate colors
+        colorMoved = "plain";
+        # Detect renamed files
+        renames = true;
+      };
+
+      push = {
+        # Push branch to the same name on remote
+        default = "simple";
+        # Automatically setup tracking branch on remote (--set-upstream origin ...)
+        autoSetupRemote = true;
+      };
+
+      fetch = {
+        # Delete branches and tags that were deleted on remote
+        prune = true;
+        pruneTags = true;
+        # Always fetch all
+        all = true;
+      };
+
+      # Attempt to correct mistyped commands, but prompt before running
+      help.autocorrect = "prompt";
+
+      # Reuse recorded resolutions
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
     };
   };
 }
