e89b03e780776db57344289921e89c1ffb706b9d
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2025-04-26 15:25:04 +0200

    fix(ssh): auto add ssh key to agent

--- hosts/common/optional/ssh.nix
+++ hosts/common/optional/ssh.nix
@@ -1,6 +1,11 @@
-_: {
+{ config, ... }:
+{
   programs.ssh = {
     enable = true;
+    addKeysToAgent = "yes";
+    extraConfig = ''
+      IdentityFile ${config.home.homeDirectory}/.ssh/id_ed25519
+    '';
     matchBlocks = {
       # "ekman" = {
       #   hostname = "ekman.oceanbox.io";
