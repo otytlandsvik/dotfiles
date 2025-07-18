6f44730d261f0200265c7944df3c25bc6f5ac17d
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2025-07-18 10:44:23 +0200

    feat(tmux): custom status config
    remove broken catppuccin plugin and config the status bar myself instead

--- hosts/common/optional/tmux.nix
+++ hosts/common/optional/tmux.nix
@@ -12,18 +12,6 @@
     mouse = true;
     plugins = with pkgs.tmuxPlugins; [
       vim-tmux-navigator
-      {
-        plugin = catppuccin;
-        extraConfig = ''
-          # Display window name instead of path
-          set -g @catppuccin_window_default_text "#W"
-          set -g @catppuccin_window_current_text "#W"
-          # Customize status modules
-          set -g @catppuccin_date_time_icon ""
-          set -g @catppuccin_date_time_text "%H:%M"
-          set -g @catppuccin_status_modules_right "session directory date_time"
-        '';
-      }
     ];
     extraConfig = ''
       # Keybinds
@@ -33,6 +21,22 @@
       # True color
       set -g default-terminal 'tmux-256color'
       set -as terminal-overrides ',alacritty*:Tc'
+
+      ### Styles ###
+
+      # Window Status
+      setw -g window-status-current-style 'fg=black bg=red'
+      setw -g window-status-current-format ' #I #W #F '
+
+      setw -g window-status-style 'fg=red bg=black'
+      setw -g window-status-format ' #I #[fg=white]#W #[fg=yellow]#F '
+
+      # Left Status
+      set -g status-left ' #[fg=blue][#S] '
+
+      # Right Status
+      set -g status-right '#[bg=green fg=black] λ #{pane_title} #[bg=blue fg=black]  %H:%M '
+      set -g status-right-length 50
     '';
   };
 }
